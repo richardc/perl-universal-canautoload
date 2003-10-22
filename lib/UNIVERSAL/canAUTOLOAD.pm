235bee187ec900b149d374ec78faac122adf3e56
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-10-22 19:22:17 +0000

    docs
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/UNIVERSAL-canAUTOLOAD@1742 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/UNIVERSAL/canAUTOLOAD.pm
+++ lib/UNIVERSAL/canAUTOLOAD.pm
@@ -24,3 +24,49 @@ sub UNIVERSAL::can {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+UNIVERSAL::canAUTOLOAD - installs a UNIVERSAL::can that respects AUTOLOAD subs
+
+=head1 SYNOPSIS
+
+ use UNIVERSAL::canAUTOLOAD;
+
+
+ package MyModule;
+
+ sub DESTROY {}
+ sub AUTOLOAD {
+     our $AUTOLOAD;
+     print "in AUTOLOAD for $AUTOLOAD\n";
+ }
+
+ my $object = bless {}, 'MyModule';
+ my $method = $object->can( 'potato' ); # returns a true value
+ $object->$method;                      # call the AUTOLOADed potato method
+
+=head1 DESCRIPTION
+
+Ever flying in the face of common sense, this module makes a special
+effort to make a section of L<UNIVERSAL/can> false.
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net> original need and documentation
+from Mark Fowler.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp. All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<UNIVERSAL/can>
+
+=cut
